3c155800c9f00c490dda22c17105af1a06a323a2
helloword4381 <jiangflow@foxmail.com>
2026-08-03 17:42:05 +0800

    feat: 添加资源定义和项目属性表

---
+++ TextStyleMgr/resource.h
@@ -0,0 +1,75 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TextStyleMgr.rc
+
+// 对话框资源ID
+#define IDD_TEXTSTYLE_MGR_DIALOG           101
+
+// 左侧 - 样式列表区域
+#define IDC_STYLE_LIST                     1001
+#define IDC_CURRENT_STYLE                  1002
+#define IDC_BTN_SET_CURRENT                1003
+#define IDC_BTN_NEW_STYLE                  1004
+#define IDC_BTN_RENAME_STYLE               1005
+#define IDC_BTN_DELETE_STYLE               1006
+#define IDC_BTN_CLEAN_STYLE                1007
+
+// 右侧 - 样式信息编辑区域
+#define IDC_EDIT_STYLE_NAME                1010
+#define IDC_EDIT_FONT_FILE                 1011
+#define IDC_EDIT_BIG_FONT                  1012
+#define IDC_EDIT_TEXT_HEIGHT               1013
+#define IDC_EDIT_WIDTH_FACTOR              1014
+#define IDC_BTN_MODIFY_STYLE               1015
+
+// 批量替换文字样式字体区域
+#define IDC_BATCH_STYLE_LIST               1020
+#define IDC_BTN_BATCH_SELECT_ALL           1021
+#define IDC_BTN_BATCH_CLEAR                1022
+#define IDC_EDIT_KEYWORD                   1023
+#define IDC_CHECK_FUZZY                    1024
+#define IDC_EDIT_BATCH_FONT                1025
+#define IDC_EDIT_BATCH_BIGFONT             1026
+#define IDC_BTN_BATCH_REPLACE              1027
+
+// 批量统一文字样式区域
+#define IDC_COMBO_TARGET_STYLE             1030
+#define IDC_BTN_UNIFY_STYLE                1031
+#define IDC_BTN_UNIFY_SELECTION            1032
+
+// 批量替换缺失字体区域
+#define IDC_EDIT_MISSING_FONT              1040
+#define IDC_EDIT_MISSING_BIGFONT           1041
+#define IDC_EDIT_MISSING_TTF               1042
+#define IDC_COMBO_FONT_CHOICE              1043
+#define IDC_CHECK_REMEMBER                 1044
+#define IDC_BTN_REPLACE_MISSING            1045
+#define IDC_CHECK_AUTO_EXECUTE             1046
+
+// 底部按钮
+#define IDC_BTN_CLOSE                      1050
+#define IDC_BTN_HELP                       1051
+#define IDC_BTN_MORE_PLUGINS               1052
+
+// 新建样式对话框
+#define IDD_NEW_STYLE_DIALOG               110
+#define IDC_NEW_STYLE_NAME                 1101
+#define IDC_NEW_FONT_FILE                  1102
+#define IDC_NEW_BIG_FONT                   1103
+#define IDC_NEW_TEXT_HEIGHT                1104
+#define IDC_NEW_WIDTH_FACTOR               1105
+
+// 帮助对话框
+#define IDD_HELP_DIALOG                    111
+#define IDC_HELP_TEXT                      1106
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE           112
+#define _APS_NEXT_COMMAND_VALUE            40001
+#define _APS_NEXT_CONTROL_VALUE            1100
+#define _APS_NEXT_SYMED_VALUE              112
+#endif
+#endif
